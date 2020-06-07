2acb0fd75205ce96cfe93c77438abd50c0ebb785
NeftXx <ronaldnef1996@gmail.com>
2020-06-06 22:49:48 -0600

    Grafica Ram 1.0

--- www/main.go
+++ www/main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	socketio "github.com/googollee/go-socket.io"
+	"github.com/shirou/gopsutil/mem"
 )
+
+type Ram struct {
+	Used        uint64 `json:"used"`
+	UsedPercent string `json:"usedPercent"`
+}
 
 func main() {
 	server, err := createServerSocket()
 
 	if err != nil {
 		log.Fatal(err)
-		return
 	}
 
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/socket.io", server)
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./public")))
 	log.Println("Server on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
@@ -31,13 +38,25 @@ func createServerSocket() (*socketio.Server, error) {
 
 	server.OnConnect("/", func(s socketio.Conn) error {
 		s.SetContext("")
-		log.Println("connected:", s.ID())
+		log.Println("A new user connected")
 		return nil
 	})
 
 	server.OnEvent("/", "cpu", func(s socketio.Conn, msg string) {})
 
-	server.OnEvent("/", "ram", func(s socketio.Conn, msg string) {})
+	server.OnEvent("/", "ram-used", func(s socketio.Conn, msg string) {
+		v, _ := mem.VirtualMemory()
+		ram := Ram{
+			Used:        v.Free / 1024 / 1024,
+			UsedPercent: fmt.Sprintf("%.4f", v.UsedPercent),
+		}
+		s.Emit("ram-used", ram)
+	})
+
+	server.OnEvent("/", "ram-total", func(s socketio.Conn, msg string) {
+		v, _ := mem.VirtualMemory()
+		s.Emit("ram-total", v.Total/1024/1024)
+	})
 
 	server.OnDisconnect("/", func(s socketio.Conn, msg string) {})
 
---
+++ www/public/js/ram.js
@@ -0,0 +1,59 @@
+const socket = io();
+
+const graph = document.getElementById("graph");
+
+Plotly.plot(
+  graph,
+  [
+    {
+      y: [0],
+      mode: "lines+markers",
+      name: "RAM consumida (MB)",
+      marker: { color: "orange", size: 8 },
+      line: { width: 4 },
+    },
+  ],
+  {
+    xaxis: {
+      title: "Uso de memoria",
+      range: [0, 10]
+    },
+    yaxis: {
+      title: "Memoria RAM (MB)",
+    },
+  }
+);
+
+socket.on("ram-total", (total) => {
+  Plotly.relayout(graph, {
+    yaxis: {
+      range: [0, total]
+    }
+  })
+});
+
+socket.on("ram-used", (ram) => {
+  Plotly.extendTraces(
+    graph,
+    {
+      y: [[ram.used]],
+    },
+    [0]
+  );
+});
+
+socket.emit("ram-total");
+
+var cnt = 0;
+
+setInterval(() => {
+  socket.emit("ram-used");
+  cnt++;
+  if (cnt > 10) {
+    Plotly.relayout(graph, {
+      xaxis: {
+        range: [cnt - 10, cnt]
+      }
+    });
+  }
+}, 1000);
