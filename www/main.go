5698ea73e4aefb5f9da2845a5332b9259b98bd1a
Jefferson Linares - 201504448 <diegoforlan102010@hotmail.com>
2020-06-06 22:53:23 -0600

    v1.0.0

--- www/main.go
+++ www/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"fmt"
 	"net/http"
-
+	"github.com/shirou/gopsutil/cpu"
 	socketio "github.com/googollee/go-socket.io"
 )
+type Cpu struct {
+	Used int         `json:"Used"`
+	UsedPercent string `json:"UsedPercent"`
+}
+
 
 func main() {
 	server, err := createServerSocket()
@@ -18,7 +25,10 @@ func main() {
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/socket.io", server)
+
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.Handle("/socket.io/", server)
 	log.Println("Server on port 3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
@@ -35,10 +45,22 @@ func createServerSocket() (*socketio.Server, error) {
 		return nil
 	})
 
-	server.OnEvent("/", "cpu", func(s socketio.Conn, msg string) {})
+	server.OnEvent("/", "cpu", func(s socketio.Conn, msg string) {
+		  log.Println("CPU")
+		  
+		  v, _ := cpu.Percent(0, false)
+		  //disk := DiskUsage("/")
+		  //log.Println(v[0])
+		  json_bytes, _ := json.Marshal(Cpu{Used: int(v[0]), 
+			UsedPercent: fmt.Sprintf("%.4f", v[0])})
+		  s.Emit("cpu", string(json_bytes))
+
+		  log.Println(json_bytes)
+
+	})
 
 	server.OnEvent("/", "ram", func(s socketio.Conn, msg string) {})
-
+	
 	server.OnDisconnect("/", func(s socketio.Conn, msg string) {})
 
 	return server, nil
