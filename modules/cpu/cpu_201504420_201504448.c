dac3aaafef86df8be817bb496512e1f5963589b5
NeftXx <ronaldnef1996@gmail.com>
2020-06-11 16:12:21 -0600

    Tabs

--- modules/cpu/cpu_201504420_201504448.c
+++ modules/cpu/cpu_201504420_201504448.c
@@ -60,7 +60,7 @@ void read_process(struct seq_file *file, struct task_struct *task_parent, int ta
   print_tabs(file, tabs);
   seq_printf(
     file,
-    "├── PID:%d\t\tNombre: %s\t\tEstado: %s\n",
+    "├── PID: %d \t\t Nombre: %s \t\t Estado: %s\n",
     task_parent->pid, task_parent->comm, state
   );
 
